ca9eca5fad77d950e09a225bcabb8077df074136
Amolpratap Singh <amolsingh.singh23@gmail.com>
2024-04-12 22:37:53 +0530

    007 Solution

---
+++ 007_10001st_Prime/README.md
@@ -0,0 +1,5 @@
+[Project Euler #7: 100001st](https://www.hackerrank.com/contests/projecteuler/challenges/euler007/problem)
+
+By listing the first six prime numbers: $2, 3, 5, 7, 11$, and $13$, we can see that the $6$th prime is $13$.
+
+What is the $10001$ st prime number?
---
+++ 007_10001st_Prime/Solution.py
@@ -0,0 +1,23 @@
+import sys
+N = 10000
+prime_num = [True for _ in range(N+1)]
+
+p = 2
+
+while p*p <= N:
+    if (prime_num[p] == True):
+        for i in range(p * p, N+1, p):
+            prime_num[i] = False
+    p += 1
+
+prime = list()
+
+for i in range(1, len(prime_num)):
+    if (prime_num[i] == True):
+        prime.append(i)
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(prime[n])
+    
